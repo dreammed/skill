2a5047e5e8064e79e8f13b88526db742cd4762b4
Kanniti Phajuang <508347@pea.co.th>
2022-11-28 10:01:24 +0700

    add asset

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layout.main')
 
 @section('content')
-{{$user}}
+    <div class="card">
+        <div class="card-header">
+            Home
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            {{ $user }}
+        </div>
+    </div>
+    <div class="mt-5 bg-light p-5 rounded">
+        <h1>Navbar example</h1>
+        <p class="lead"> {{ $user }} </p>
+        <a class="btn btn-lg btn-primary" href="../components/navbar/" role="button">View navbar docs &raquo;</a>
+    </div>
 @endsection
